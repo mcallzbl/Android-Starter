da2b181965a8211ab02cacc30975576d2b1809b1
mcallzbl <mcallzbl@qq.com>
2025-04-20 16:20:28 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,20 @@
+ksp
+hilt
+
+Timber日志库
+自定义App类 并初始化Timber
+
+开启BuildConfig
+compileSdk = 35
+
+retrofit
+
+room
+
+gson
+
+datastore
+
+material.icons.extended
+
+创建了BaseActivity 并启用高刷新率
---
+++ app/src/main/java/com/mcallzbl/android_starter/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.mcallzbl.android_starter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mcallzbl.android_starter.base.BaseActivity
+import com.mcallzbl.android_starter.ui.theme.AndroidstarterTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidstarterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidstarterTheme {
+        Greeting("Android")
+    }
+}
