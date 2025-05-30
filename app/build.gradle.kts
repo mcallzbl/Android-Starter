5399fa447853be50d04c90481018b1205f2169c5
mcallzbl <mcallzbl@qq.com>
2025-05-30 19:59:32 +0800

    引入kotlin序列化依赖与插件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -60,6 +61,8 @@ android {
     }
 }
 dependencies {
+    // Navigation Compose
+    implementation(libs.kotlinx.serialization.json)
     //网络请求
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
