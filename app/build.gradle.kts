62a9d16c27cbf7d8b7f47fd86e7823ca02554e8e
mcallzbl <mcallzbl@qq.com>
2026-02-14 10:24:40 +0800

    开启混淆和代码缩减

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
